25975e9ca612acd154c9e25372d5b6fd38a5ed34
Yegansmaili <yegansmaili@gmail.com>
2024-03-13 23:39:03 +0330

    add tests

--- accounts/tests.py
+++ accounts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SignUpTest(TestCase):
+    username = 'newusertest'
+    email = 'newusertest@gmail.com'
+    password = 'newusertest@y'
+
+    def test_signup_url(self):
+        res = self.client.get('/accounts/signup/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_signup_url_by_name(self):
+        res = self.client.get(reverse('signup'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_signup_form(self):
+        # custom_user = get_user_model()
+        get_user_model().objects.create_user(
+            self.username,
+            self.email,
+            self.password,
+        )
+        self.assertEqual(get_user_model().objects.all().count(), 1)
+        self.assertEqual(get_user_model().objects.all()[0].username, self.username)
+        self.assertEqual(get_user_model().objects.all()[0].email, self.email)
--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,20 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class HomepageTest(TestCase):
+    def test_home_page_url(self):
+        res = self.client.get('/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_home_page_url_by_name(self):
+        res = self.client.get(reverse('home'))
+        self.assertEqual(res.status_code, 200)
+
+    def test_home_page_content(self):
+        res = self.client.get(reverse('home'))
+        self.assertContains(res, 'Home page')
+
+    def test_home_page_template_used(self):
+        res = self.client.get(reverse('home'))
+        self.assertTemplateUsed(res, 'home.html')
