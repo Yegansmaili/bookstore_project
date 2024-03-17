d643fe482fe274716d36920c6f4f48aac1ad7fdb
Yegansmaili <yegansmaili@gmail.com>
2024-03-17 22:42:03 +0330

    add frontend to my home page

--- config/settings.py
+++ config/settings.py
@@ -115,11 +115,7 @@ USE_I18N = True
 
 USE_TZ = True
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -139,3 +135,10 @@ CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 # email_settings
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.0/howto/static-files/
+
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [str(BASE_DIR.joinpath('static'))]
+
--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.views import generic
+from books.models import Book
 
 
 class HomePageView(generic.TemplateView):
+
+    def get_context_data(self, **kwargs):
+        kwargs['books'] = Book.objects.all().order_by('-created_at')
+        return super().get_context_data(**kwargs)
+
     template_name = 'home.html'
